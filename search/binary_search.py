4e402962f407541194d9595422155ab1d85c615f
Lonik <l0nik@mail.ru>
2020-04-03 20:37:45 +0300

    created file 'binary_search_test.py' with some unit tests

---
+++ search/binary_search.py
@@ -0,0 +1,19 @@
+def solve(input_list, item):
+
+    low = 0
+    high = len(input_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = input_list[mid]
+        if guess == item:
+            return mid
+        elif guess < item:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return None
+
+#print(solve([1, 2, 3, 4], 4))
+print(solve([], 4))
---
+++ search/binary_search_test.py
@@ -0,0 +1,28 @@
+import unittest
+import binary_search
+
+class BinatySearchTest(unittest.TestCase):
+    
+    def test_binary_search_lboundary(self):
+        result = binary_search.solve([1, 2, 3, 4], 1)
+        expected = 0
+        self.assertEqual(result, expected)
+
+    def test_binary_search_rboundary(self):
+        result = binary_search.solve([1, 2, 3, 4], 4)
+        expected = 3
+        self.assertEqual(result, expected)
+
+    def test_binary_search_empty_list(self):
+        result = binary_search.solve([], 4)
+        expected = None
+        self.assertEqual(result, expected)
+
+    def test_binary_search_normal(self):
+        result = binary_search.solve([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 5)
+        expected = 4
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
